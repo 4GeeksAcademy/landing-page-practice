ce785bb7dc4c2963c11c7a447eaa025dc70fe7c3
Yohan <102123553+yohanmayorga@users.noreply.github.com>
2023-08-29 13:31:48 -0400

    Update cards.jsx

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -7,7 +7,7 @@ const Cards = (props) => {
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.text}</p>
-                <a href="#" className="../img/rigo-baby.jpg">{props.label}</a>
+                <a href="#" className="btn btn-primary">{props.label}</a>
             </div>
         </div>
     );
